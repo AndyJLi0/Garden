18ef5b8e14e52feb44efe59731aa1af6ebce2471
Jim Geng <jimgeng2004@gmail.com>
2025-01-19 11:54:45 -0800

    map

--- frontend/app/(two)/map2.tsx
+++ frontend/app/(two)/map2.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet ,  TouchableOpacity,} from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect, useMemo, useRef } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { LineLayer, MapView, ShapeSource } from "@rnmapbox/maps";
+import Mapbox from "@rnmapbox/maps";
 import {
   appBeige,
   header1size,
@@ -8,36 +9,49 @@ import {
   textPrimary,
   textSecondary,
 } from "../../utilities/themeColors";
-import { convertMinutesToHoursAndMinutes } from "../../utilities/convertMinToHandM";
 import { OPEN_WEATHER_API_KEY } from "./apiKey";
-import { parseWeatherResponse, parseLocationResponse } from "../../utilities/parseInfo";
+import {
+  parseWeatherResponse,
+  parseLocationResponse,
+} from "../../utilities/parseInfo";
 import GetLocation from "react-native-get-location";
+import {
+  LocationAccuracy,
+  LocationCallback,
+  LocationSubscription,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+} from "expo-location";
+import { calcDistance } from "../../utilities/recordUtils";
+
+type LatLng = {
+  latitude: number;
+  longitude: number;
+};
 
 const user = {
   name: "Jim",
   session: {
     walked: 30,
     time: 150,
-    steps: 200
+    steps: 200,
   },
 };
 
-
-// var previouslat = 0;
-// var previouslon = 0;
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoiamltZ2VuZyIsImEiOiJjbG42aWpsZ3QwM3h1Mm5vNmxlNzc0Y3UwIn0.oYMH6baz1zvwQqFrP6pQtQ"
+);
 
 const callDuringRecord = () => {
   //
-}
-
+};
 
 const INITIAL_REGION = {
   latitude: 49.31,
   longitude: -123.2,
   latitudeDelta: 0.005,
-  longitudeDelta: 0.005
+  longitudeDelta: 0.005,
 };
-
 
 const requestLocation = async () => {
   const coords = await GetLocation.getCurrentPosition({
@@ -59,29 +73,51 @@ export default function Map(): JSX.Element {
   const [weatherData, setWeatherData] = useState<any>(null);
   const [weatherLoading, setWeatherLoading] = useState<boolean>(false);
   const [weatherError, setWeatherError] = useState<string | null>(null);
+  const [polylineLatLongs, setPolylineLatLongs] = useState<LatLng[]>([]);
 
   // for timer
   const [start, setStart] = useState(false);
   const [count, setCount] = useState(0);
   const [time, setTime] = useState("00:00:00");
+  const watchLocationRef = useRef<LocationSubscription | null>(null);
+
   var initTime = new Date();
 
+  const updateDistanceAndPolyline: LocationCallback = ({ coords }) => {
+    const { latitude, longitude } = coords;
+    setPolylineLatLongs((prev) => prev.concat([{ latitude, longitude }]));
+  };
 
-  const startActivity = () => {
-    // start the timer. starts the periodic location update.
-    setStart(true);
-  }
+  const startActivity = async () => {
+    const response = await requestForegroundPermissionsAsync();
+    if (response.granted) {
+      // start the timer. starts the periodic location update.
+      watchLocationRef.current = await watchPositionAsync(
+        {
+          accuracy: LocationAccuracy.BestForNavigation,
+          distanceInterval: 0,
+          timeInterval: 5000,
+        },
+        updateDistanceAndPolyline
+      );
+      setStart(true);
+    }
+  };
 
   const pauseActivity = () => {
     // pauses the timer. pauses the periodic location update.
+    watchLocationRef.current?.remove();
     setStart(false);
-
-  }
+  };
 
   const finishActivity = () => {
     // stops timer, saves data to an activity. stops the periodic location update.
+    watchLocationRef.current?.remove();
+    // TODO SAVING
+
+    setPolylineLatLongs([]);
     clearTime();
-  }
+  };
   // how the timer should look like
   const showTimer = (ms: number) => {
     const milliseconds = Math.floor((ms % 1000) / 10)
@@ -93,15 +129,27 @@ export default function Map(): JSX.Element {
     const minute = Math.floor((ms / 1000 / 60) % 60)
       .toString()
       .padStart(2, "0");
-    setTime(
-      minute + ":" + second + ":" + milliseconds
-    );
+    setTime(minute + ":" + second + ":" + milliseconds);
   };
   // sets time to zero
   const clearTime = () => {
     setTime("00:00:00");
     setCount(0);
   };
+
+  const distance = polylineLatLongs.reduce((acc, curr, i, arr) => {
+    if (i === 0) {
+      return 0;
+    }
+    const prev = arr[i - 1];
+    const distance = calcDistance(
+      prev.latitude,
+      prev.longitude,
+      curr.latitude,
+      curr.longitude
+    );
+    return acc + distance;
+  }, 0);
 
   // update timer
   useEffect(() => {
@@ -119,7 +167,6 @@ export default function Map(): JSX.Element {
     }, 10); //10 ms
     return () => clearInterval(id);
   }, [start]);
-
 
   // somewhere in spain
   var lat = 41.40338;
@@ -153,15 +200,33 @@ export default function Map(): JSX.Element {
     }
   };
 
+  const coords = useMemo<GeoJSON.Geometry>(
+    () => ({
+      type: "LineString",
+      coordinates: polylineLatLongs.map((point) => [
+        point.longitude,
+        point.latitude,
+      ]),
+    }),
+    [polylineLatLongs]
+  );
+
   return (
     <View style={styles.container}>
-
       <Text style={styles.title}>Map</Text>
-      <MapView
-        style={styles.map}
-        initialRegion={INITIAL_REGION}
-        showsUserLocation
-      />
+      <MapView style={styles.map}>
+        {polylineLatLongs.length > 1 ? (
+          <ShapeSource id="lineSource" shape={coords}>
+            <LineLayer
+              id="lineLayer"
+              style={{
+                lineWidth: 2,
+                lineColor: "#ff0000",
+              }}
+            />
+          </ShapeSource>
+        ) : null}
+      </MapView>
 
       <Text
         style={{
@@ -177,7 +242,6 @@ export default function Map(): JSX.Element {
         <Button title="Get Weather (remove later)" onPress={requestWeather} />
       </View> */}
 
-      
       <View style={{ flexDirection: "row" }}>
         <Text
           style={{
@@ -195,7 +259,7 @@ export default function Map(): JSX.Element {
             color: textSecondary,
           }}
         >
-          {user.session.walked}km
+          {distance}km
         </Text>
       </View>
       <View
@@ -244,8 +308,13 @@ export default function Map(): JSX.Element {
         </Text>
       </View>
 
-
-      <View style={{ flexDirection: "row", justifyContent: "space-between", marginVertical: 20 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginVertical: 20,
+        }}
+      >
         {/* Start Button */}
         {start ? (
           <TouchableOpacity style={styles.button} onPress={pauseActivity}>
@@ -258,7 +327,10 @@ export default function Map(): JSX.Element {
         )}
 
         {/* Finish Button */}
-        <TouchableOpacity style={[styles.button, styles.finishButton]} onPress={finishActivity}>
+        <TouchableOpacity
+          style={[styles.button, styles.finishButton]}
+          onPress={finishActivity}
+        >
           <Text style={styles.buttonText}>Finish</Text>
         </TouchableOpacity>
       </View>
@@ -270,7 +342,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderRadius: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
     //justifyContent: 'center', //vertical align
     height: "100%",
     paddingHorizontal: 20,
@@ -278,10 +350,10 @@ const styles = StyleSheet.create({
     backgroundColor: appBeige,
   },
   map: {
-    width: '100%',
-    height: '60%',
+    width: "100%",
+    height: "60%",
     borderRadius: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
     padding: 20,
   },
   title: {
