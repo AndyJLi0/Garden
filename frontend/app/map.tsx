5231f93bf45e22437579cf57d56ee1b10ddaadfe
Paingle <andyjiaanli@gmail.com>
2025-01-18 21:06:45 -0800

    try pls

--- frontend/app/map.tsx
+++ frontend/app/map.tsx
@@ -1,39 +1,150 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React, {useState} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ActivityIndicator,
+} from 'react-native';
 
-const INITIAL_REGION = {
-	latitude: 49.33,
-	longitude: -123.2,
-	latitudeDelta: 0.00005,
-	longitudeDelta: 0.00005
-};
+import GetLocation, {
+  Location,
+  LocationErrorCode,
+  isLocationError,
+} from 'react-native-get-location';
 
-export default function Map() {
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  location: {
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    marginBottom: 8,
+  },
+});
+
+function App(): JSX.Element {
+  const [loading, setLoading] = useState(false);
+  const [location, setLocation] = useState<Location | null>(null);
+  const [error, setError] = useState<LocationErrorCode | null>(null);
+
+  const requestLocation = () => {
+    setLoading(true);
+    setLocation(null);
+    setError(null);
+
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 30000,
+      rationale: {
+        title: 'Location permission',
+        message: 'The app needs the permission to request your location.',
+        buttonPositive: 'Ok',
+      },
+    })
+      .then(newLocation => {
+        setLoading(false);
+        setLocation(newLocation);
+      })
+      .catch(ex => {
+        if (isLocationError(ex)) {
+          const {code, message} = ex;
+          console.warn(code, message);
+          setError(code);
+        } else {
+          console.warn(ex);
+        }
+        setLoading(false);
+        setLocation(null);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Map</Text>
-      <MapView 
-        style={styles.map}
-        initialRegion={INITIAL_REGION}
-        showsUserLocation
-    />
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>
+        To get location, press the button:
+      </Text>
+
+      <View style={styles.button}>
+        <Button
+          disabled={loading}
+          title="Get Location"
+          onPress={requestLocation}
+        />
+      </View>
+
+      {loading ? <ActivityIndicator /> : null}
+      {location ? (
+        <Text style={styles.location}>{JSON.stringify(location, null, 2)}</Text>
+      ) : null}
+      {error ? <Text style={styles.location}>Error: {error}</Text> : null}
+
+      <Text style={styles.instructions}>Extra functions:</Text>
+      <View style={styles.button}>
+        <Button
+          title="Open App Settings"
+          onPress={() => {
+            GetLocation.openSettings();
+          }}
+        />
+      </View>
+
+      <Text style={styles.instructions}>{instructions}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 20,
-        overflow: 'hidden',
-    },
-    map: {
-        width: '100%',
-        height: '60%',
-        borderRadius: 20,
-        overflow: 'hidden',
-    },
-});
+export default App;
+// export default function Map() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Map</Text>
+//       <MapView
+//         style={styles.map}
+//         initialRegion={INITIAL_REGION}
+//         showsUserLocation
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderRadius: 20,
+//     overflow: 'hidden',
+//   },
+//   map: {
+//     width: '100%',
+//     height: '60%',
+//     borderRadius: 20,
+//     overflow: 'hidden',
+//   },
+// });
