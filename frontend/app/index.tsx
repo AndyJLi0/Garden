00d18b9315755fe7d342da5eb595542046dd7faa
Jim Geng <jimgeng2004@gmail.com>
2025-01-18 19:09:56 -0800

    home page done (hardcode)

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,91 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import RoseBlooming from "../components/SVGs/Rose/RoseBlooming";
+import { useFonts } from "expo-font";
+import {
+  JosefinSans_400Regular,
+  JosefinSans_700Bold,
+} from "@expo-google-fonts/josefin-sans";
+import {
+  appBeige,
+  header1size,
+  header2size,
+  textPrimary,
+  textSecondary,
+} from "../utilities/themeColors";
 
 export default function Home() {
+  useFonts({
+    JosefinSans_400Regular,
+    JosefinSans_700Bold,
+  });
+
+  const user = {
+    name: "Jim",
+    session: {
+      walked: 30,
+      time: 180,
+    },
+  };
+
+  const flower = <RoseBlooming />;
+
   return (
-    <View style={{}}>
-      <Text>Home</Text>
-      <StatusBar hidden={true} />
+    <View
+      style={{
+        height: "100%",
+        paddingHorizontal: 40,
+        paddingTop: 80,
+        backgroundColor: appBeige,
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "JosefinSans_700Bold",
+          fontSize: header1size,
+          color: textPrimary,
+          textAlign: "center",
+        }}
+      >
+        {user.name}'s Garden
+      </Text>
+      <View
+        style={{
+          alignItems: "center",
+          paddingVertical: 60,
+        }}
+      >
+        {flower}
+      </View>
+      <Text
+        style={{
+          fontFamily: "JosefinSans_700Bold",
+          fontSize: header2size,
+          color: textPrimary,
+        }}
+      >
+        Today
+      </Text>
+      <Text
+        style={{
+          fontFamily: "JosefinSans_700Bold",
+          fontSize: header2size,
+          color: textSecondary,
+        }}
+      >
+        Km walked: {user.session.walked}
+      </Text>
+      <Text
+        style={{
+          fontFamily: "JosefinSans_700Bold",
+          fontSize: header2size,
+          color: textSecondary,
+        }}
+      >
+        Time: {user.session.time}
+      </Text>
+
+      <StatusBar style="inverted" />
     </View>
   );
 }
